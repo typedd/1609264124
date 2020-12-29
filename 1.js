40f39957a6c1b1e9d7045ae34f549222b926c91d
Dmitry Kouznetsov <dmitry.kouznetsov@nentgroup.net>
2020-12-29 18:56:38 +0100

    asdf

---
+++ 1.js
@@ -0,0 +1,73 @@
+// @flow
+
+// $FlowFixMe
+const createRequest = require('./createRequest');
+// $FlowFixMe
+const identityService = require('./identityService');
+
+/*::
+import type {
+    RequestParams,
+    GatewayHeaders,
+    OutsideRequestParams,
+    MtgResponse,
+    AuthHeaders,
+    CreateRequestOptions,
+    GatewayConfig,
+    AccessToken
+} from './types'
+*/
+
+const mkOutsideRequestParams/*: (RequestParams, GatewayHeaders, AuthHeaders) => OutsideRequestParams */ = 
+	(requestParams, gatewayHeaders, authHeaders) => 
+({
+	...requestParams,
+	// $FlowFixMe
+	headers: {
+		...requestParams.headers,
+		...gatewayHeaders,
+		...authHeaders
+	}
+});
+
+const mkAuthHeaders = (accessToken/*: AccessToken*/)/*: AuthHeaders */ =>
+	({ authorization: `MTG-AT ${accessToken}` });
+
+const mkGatewayHeaders = ()/*: GatewayHeaders */ => ({
+	'X-Gateway-Throttling': 'disabled'
+});
+
+// TODO: move it to identityService
+const authorize/*: (config: GatewayConfig) => Promise< AccessToken > */ = (config) =>
+	identityService.getAccessToken(config)
+	.catch((err) => {
+		identityService.removeAccessToken(config.identityName);
+		throw err;
+	});
+
+const pub/*: ({| ...CreateRequestOptions, gatewayConfig: GatewayConfig |}) => (RequestParams => Promise<MtgResponse>) */ = (config) => {
+    // $FlowFixMe
+    const request = createRequest(config);
+
+    return async (requestParams/* RequestParams */)/*: Promise<MtgResponse> */ => {
+        const accessToken = await authorize(config.gatewayConfig);
+        const outsideRequestParams = mkOutsideRequestParams(
+            requestParams, mkGatewayHeaders(), mkAuthHeaders(accessToken)
+        );
+        // $FlowFixMe
+        const response = await request(outsideRequestParams);
+        if (response.statusCode === 401) {
+            identityService.removeAccessToken(config.gatewayConfig.identityName);
+            const accessToken2 = await authorize(config.gatewayConfig);
+            const outsideRequestParams2 = mkOutsideRequestParams(
+                requestParams, mkGatewayHeaders(), mkAuthHeaders(accessToken2)
+            );
+            // $FlowFixMe
+            return request(outsideRequestParams2);
+        } else {
+            return response;
+        };
+    }
+}
+
+module.exports = pub;
